b29bff88712bf7c1d6790b41edb6c806cfb8c182
guru <gkathir15@gmail.com>
2018-12-23 00:19:28 +0530

    worked login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'constants/FirebaseConsts.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn googleSignIn = GoogleSignIn();
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseUser fireBaseUser;
 
 void main() => runApp(MyApp());
 
@@ -28,44 +29,54 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return fireBaseUser==null?Scaffold(
+      resizeToAvoidBottomPadding: false,
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text("Diary"),
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        centerTitle: true,
-      ),
-      body: Center(
-          child: new RaisedButton(
-        onPressed: () {
-          _handleSignIn()
-              .then((FirebaseUser user) => () {
-                    FirebaseConsts.fireBaseUser = user;
-                    print(user);
-                    print(FirebaseConsts.fireBaseUser);
-                  })
-              .catchError((e) => print(e));
-        },
-        color: Colors.white,
-        child: new Icon(
-          Icons.assignment_ind,
-          color: Colors.white,
+      body: Container(decoration: new BoxDecoration(image: new DecorationImage(image:new AssetImage("assets/jpgs/drew.jpg"),fit: BoxFit.cover,alignment: Alignment.center,)),
+
+        child:
+        new Center(
+          child:new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround ,
+            children: <Widget>[
+              new Text("Diary",style:const TextStyle(fontFamily: "Handlee",color: Colors.white,fontSize: 100.0,)),
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+           new Text("Sign in with",style: new TextStyle(fontSize:30,color: Colors.white,fontFamily: "bloom"),),
+           new InkWell(
+             splashColor: Colors.white24,
+             child: new Image.asset("assets/pngs/google.png",height: MediaQuery.of(context).size.width/5,width:  MediaQuery.of(context).size.width/5,),onTap:(){
+               setState(() {
+                 _handleSignIn();
+               });
+           },)
+              ],)
+            ],
+          )
         ),
-      )),
+        )
+
+
 
       // This trailing comma makes auto-formatting nicer for build methods.
+    ):Scaffold(
+      floatingActionButton: new FloatingActionButton(backgroundColor:Colors.indigoAccent,onPressed: null,child: new Icon(Icons.add,),),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      backgroundColor: Colors.black,
+      resizeToAvoidBottomPadding: false,
+      bottomNavigationBar: BottomAppBar(notchMargin: 2.0,),
     );
   }
 
-  Future<FirebaseUser> _handleSignIn() async {
-    GoogleSignInAccount googleUser = await FirebaseConsts.googleSignIn.signIn();
+   _handleSignIn() async {
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
     GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    FirebaseUser user = await FirebaseConsts.auth.signInWithGoogle(
+    FirebaseUser user = await auth.signInWithGoogle(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
     print("signed in " + user.displayName);
-    return user;
+    fireBaseUser = user;
   }
 }
